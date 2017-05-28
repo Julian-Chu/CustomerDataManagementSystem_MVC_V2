178dff99fc89dc6a6acef460e914747e22d34d82
JulianChu <yulang.chu@gmail.com>
2017-05-28 20:38:44 +0200

    add attributes in 客戶資料統計View

--- CustomerDataManagementSystem_MVC_V2/Models/客戶資料統計.Partial.cs
+++ CustomerDataManagementSystem_MVC_V2/Models/客戶資料統計.Partial.cs
@@ -1,23 +1,25 @@
 namespace CustomerDataManagementSystem_MVC_V2.Models
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(客戶資料統計MetaData))]
     public partial class 客戶資料統計
     {
     }
-    
+
     public partial class 客戶資料統計MetaData
     {
         [Required]
         public int Id { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "Max Length is 50 words")]
         [Required]
+        [Display(Name = "Customer")]
         public string 客戶名稱 { get; set; }
+        [Display(Name = "number of contacts")]
         public Nullable<int> 聯絡人數量 { get; set; }
+        [Display(Name = "number of bank accounts")]
         public Nullable<int> 銀行帳戶數量 { get; set; }
     }
 }
