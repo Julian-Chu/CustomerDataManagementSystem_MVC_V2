a857b8070eb7d2048d6a9a75f1c0ac7d4938b389
JulianChu <yulang.chu@gmail.com>
2017-05-29 18:50:10 +0200

    apply layout-1

---
+++ CustomerDataManagementSystem_MVC_V2/Controllers/Layout/LayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerDataManagementSystem_MVC_V2.Controllers.Layout
+{
+    public class LayoutController : Controller
+    {
+        // GET: Layout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomerDataManagementSystem_MVC_V2/Helpers/CustomHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerDataManagementSystem_MVC_V2.Helpers
+{
+    public class ActionLinkWithIconcs
+    {
+    }
+}
